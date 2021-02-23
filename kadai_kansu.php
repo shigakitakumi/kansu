e2a1a7a6b82c5541091475e22d2322d09c521646
takumi <free.swim.0723@icloud.com>
2021-02-23 13:39:28 +0000

    課題提出

---
+++ kadai_kansu.php
@@ -0,0 +1,34 @@
+<?php
+function sum($max){
+    $result = 0;
+    for($i = 1; $i <= $max; $i++){
+        $result += $i;
+    }
+    return $result *2;
+}
+echo sum(10);
+
+function f($a, $b){
+    return $a + $b;
+}
+echo add(3,5);
+
+function multiple($arr){
+    $result = 1;
+    foreach($arr as $num){
+        echo  
+    }
+    return $result;
+}
+echo multipule([1,3,5,7,9]);
+
+ function max_array($arr){
+ $max_number = $arr[0];
+ foreach($arr as $a){
+     
+     }
+ }
+
+ return $max_number;
+ }
+?>
